60b956629b0746918383d327a7e133c183aa8885
Sean Pianka <pianka@eml.cc>
2017-08-01 04:14:27 -0400

    add filter_by, list_all, and zips= parameter to functions for greater searching/filtering capabilities

--- README.rst
+++ README.rst
@@ -19,13 +19,30 @@ supported functionality.
 
 .. code:: python
 
+    >>> from pprint import pprint
     >>> import zipcodes
 
-    >>> print(zipcodes.matching('06469'))
-    [{'zip_code': '06469', 'zip_code_type': 'STANDARD', 'city': 'MOODUS', 'state': 'CT', 'lat': 41.5, 'long': -72.45, 'world_region': 'NA', 'country': 'US', 'active': True}]
+    >>> pprint(zipcodes.matching('77429'))
+    [{'zip_code': '77429',
+      'zip_code_type': 'STANDARD',
+      'city': 'CYPRESS',
+      'state': 'TX',
+      'lat': 29.96,
+      'long': -95.69,
+      'world_region': 'NA',
+      'country': 'US',
+      'active': True}]
 
-    >>> print(zipcodes.matching('06469-1145'))
-    [{'zip_code': '06469', 'zip_code_type': 'STANDARD', 'city': 'MOODUS', 'state': 'CT', 'lat': 41.5, 'long': -72.45, 'world_region': 'NA', 'country': 'US', 'active': True}]
+    >>> pprint(zipcodes.matching('77429-1145'))
+    [{'zip_code': '77429',
+      'zip_code_type': 'STANDARD',
+      'city': 'CYPRESS',
+      'state': 'TX',
+      'lat': 29.96,
+      'long': -95.69,
+      'world_region': 'NA',
+      'country': 'US',
+      'active': True}]
 
     >>> print(zipcodes.matching('06463'))
     []
@@ -36,6 +53,78 @@ supported functionality.
     >>> print(zipcodes.is_valid('06469'))
     True
 
-    >>> print(zipcodes.similar_to('0646'))
-    [{'zip_code': '06460', 'zip_code_type': 'STANDARD', 'city': 'MILFORD', 'state': 'CT', 'lat': 41.22, 'long': -73.06, 'world_region': 'NA', 'country': 'US', 'active': True}, {'zip_code': '06461', 'zip_code_type': 'STANDARD', 'city': 'MILFORD', 'state': 'CT', 'lat': 41.23, 'long': -73.08, 'world_region': 'NA', 'country': 'US', 'active': True}, {'zip_code': '06467', 'zip_code_type': 'PO BOX', 'city': 'MILLDALE', 'state': 'CT', 'lat': 41.61, 'long': -72.87, 'world_region': 'NA', 'country': 'US', 'active': True}, {'zip_code': '06468', 'zip_code_type': 'STANDARD', 'city': 'MONROE', 'state': 'CT', 'lat': 41.36, 'long': -73.2, 'world_region': 'NA', 'country': 'US', 'active': True}, {'zip_code': '06469', 'zip_code_type': 'STANDARD', 'city': 'MOODUS', 'state': 'CT', 'lat': 41.5, 'long': -72.45, 'world_region': 'NA', 'country': 'US', 'active': True}]
+    >>> pprint(zipcodes.similar_to('0643'))
+    [{'active': True,
+      'city': 'GUILFORD',
+      'country': 'US',
+      'lat': 41.28,
+      'long': -72.67,
+      'state': 'CT',
+      'world_region': 'NA',
+      'zip_code': '06437',
+      'zip_code_type': 'STANDARD'},
+     {'active': True,
+      'city': 'HADDAM',
+      'country': 'US',
+      'lat': 41.45,
+      'long': -72.5,
+      'state': 'CT',
+      'world_region': 'NA',
+      'zip_code': '06438',
+      'zip_code_type': 'STANDARD'},
+    ... # remaining results truncated for readability...
+    ]
+
+    >>> pprint(zipcodes.filter_by(zipcodes.list_all(), active=True, city='WINDSOR'))
+    [{'active': True,
+      'city': 'WINDSOR',
+      'country': 'US',
+      'lat': 44.31,
+      'long': -69.58,
+      'state': 'ME',
+      'world_region': 'NA',
+      'zip_code': '04363',
+      'zip_code_type': 'STANDARD'},
+     {'active': True,
+      'city': 'WINDSOR',
+      'country': 'US',
+      'lat': 43.48,
+      'long': -72.42,
+      'state': 'VT',
+      'world_region': 'NA',
+      'zip_code': '05089',
+      'zip_code_type': 'STANDARD'},
+    ... # remaining results truncated for readability...
+    ]
+
+    >>> pprint(zipcodes.similar_to('2', zips=zipcodes.filter_by(zipcodes.list_all(), active=True, city='WINDSOR')))
+    [{'active': True,
+      'city': 'WINDSOR',
+      'country': 'US',
+      'lat': 33.48,
+      'long': -81.51,
+      'state': 'SC',
+      'world_region': 'NA',
+      'zip_code': '29856',
+      'zip_code_type': 'STANDARD'},
+     {'active': True,
+      'city': 'WINDSOR',
+      'country': 'US',
+      'lat': 36.8,
+      'long': -76.73,
+      'state': 'VA',
+      'world_region': 'NA',
+      'zip_code': '23487',
+      'zip_code_type': 'STANDARD'},
+     {'active': True,
+      'city': 'WINDSOR',
+      'country': 'US',
+      'lat': 36.0,
+      'long': -76.94,
+      'state': 'NC',
+      'world_region': 'NA',
+      'zip_code': '27983',
+      'zip_code_type': 'STANDARD'}]
+
+
 
--- zipcodes/__init__.py
+++ zipcodes/__init__.py
@@ -18,19 +18,20 @@ __email__ = 'pianka@eml.cc'
 __license__ = 'MIT'
 __package__ = 'zipcodes'
 __version__ = '1.0.0'
+__all__ = ['matching', 'similar_to', 'is_valid']
 
 _digits = re.compile('[^\d\-]')
 
 with gzip.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'zips.json.gz'), 'rb') as f:
-    zips = json.load(f)
+    _zips = json.load(f)
 
 
 def validated_zipcode(f):
-    return lambda zipcode: f(_validate(zipcode))
+    return lambda zipcode, *args, **kwargs: f(_validate(zipcode), *args, **kwargs)
 
 
 @validated_zipcode
-def matching(zipcode):
+def matching(zipcode, zips=_zips):
     '''
     Retrieve additional information about a zip-code such as the associated
     city and state. The provided zipcode can either be of the form '06469' or
@@ -47,12 +48,16 @@ def matching(zipcode):
         Dictionary containing "zip_code", "zip_code_type", "city", "state",
         "world_region", and "country" as keys. Example:
 
-        >>> {"zip_code": "00705",
-             "zip_code_type": "STANDARD",
-             "city": "AIBONITO",
-             "state": "PR",
-             "world_region": "NA",
-             "country": "US"}
+        >>> zipcodes.matching('77429')
+        [{'zip_code': '77429',
+          'zip_code_type': 'STANDARD',
+          'city': 'CYPRESS',
+          'state': 'TX',
+          'lat': 29.96,
+          'long': -95.69,
+          'world_region': 'NA',
+          'country': 'US',
+          'active': True}]
 
     Raises
     ------
@@ -80,6 +85,10 @@ def is_valid(zipcode):
     bool
         Whether the provided zip-code is valid.
 
+    >>> zipcodes.is_valid('77429')
+    True
+
+
     Raises
     ------
     TypeError
@@ -90,7 +99,7 @@ def is_valid(zipcode):
 
 
 @validated_zipcode
-def similar_to(partial_zipcode):
+def similar_to(partial_zipcode, zips=_zips):
     '''
     Retrieve a list of zip-code dictionaries where the prefix matches the
     provided partial zip-code.
@@ -106,12 +115,34 @@ def similar_to(partial_zipcode):
         List of dictionaries containing "zip_code", "zip_code_type", "city",
         "state", "world_region", and "country" as keys. Example:
 
-        >>> {"zip_code": "00705",
-             "zip_code_type": "STANDARD",
-             "city": "AIBONITO",
-             "state": "PR",
-             "world_region": "NA",
-             "country": "US"}
+        >>> zipcodes.similar_to('0643')
+        [{'active': True,
+          'city': 'GUILFORD',
+          'country': 'US',
+          'lat': 41.28,
+          'long': -72.67,
+          'state': 'CT',
+          'world_region': 'NA',
+          'zip_code': '06437',
+          'zip_code_type': 'STANDARD'},
+         {'active': True,
+          'city': 'HADDAM',
+          'country': 'US',
+          'lat': 41.45,
+          'long': -72.5,
+          'state': 'CT',
+          'world_region': 'NA',
+          'zip_code': '06438',
+          'zip_code_type': 'STANDARD'},
+         {'active': True,
+          'city': 'HADLYME',
+          'country': 'US',
+          'lat': 41.4,
+          'long': -72.34,
+          'state': 'CT',
+          'world_region': 'NA',
+          'zip_code': '06439',
+          'zip_code_type': 'PO BOX'}]
 
     Raises
     ------
@@ -120,6 +151,78 @@ def similar_to(partial_zipcode):
         digits and "-".
     '''
     return [z for z in zips if z['zip_code'].startswith(partial_zipcode)]
+
+
+def filter_by(zips, **kwargs):
+    """
+    Filter through a list of zip-codes by providing keyword arguments and the
+    values which should be filtered.
+
+    Parameters
+    ----------
+    zips : list of dict
+        A list containing each dictionary object with which to filter through.
+    kwargs : dict
+        A dictionary containing pairs where the key is the attribute to filter
+        by, and its corresponding value is the value to filter for the
+        aforementioned key.
+
+    Returns
+    -------
+    list of dict
+        A list containing each dictionary object which was filtered.
+
+    >>> pprint(zipcodes.similar_to('2', zips=zipcodes.filter_by(zipcodes.list_all(), active=True, city='WINDSOR')))
+    [{'active': True,
+      'city': 'WINDSOR',
+      'country': 'US',
+      'lat': 33.48,
+      'long': -81.51,
+      'state': 'SC',
+      'world_region': 'NA',
+      'zip_code': '29856',
+      'zip_code_type': 'STANDARD'},
+     {'active': True,
+      'city': 'WINDSOR',
+      'country': 'US',
+      'lat': 36.8,
+      'long': -76.73,
+      'state': 'VA',
+      'world_region': 'NA',
+      'zip_code': '23487',
+      'zip_code_type': 'STANDARD'},
+     {'active': True,
+      'city': 'WINDSOR',
+      'country': 'US',
+      'lat': 36.0,
+      'long': -76.94,
+      'state': 'NC',
+      'world_region': 'NA',
+      'zip_code': '27983',
+      'zip_code_type': 'STANDARD'}]
+
+
+    """
+    results = zips
+    for k, v in kwargs.items():
+        results = [r for r in results if r.get(k) == v]
+    return results
+
+
+def list_all(zips=_zips):
+    """
+    Return a list containing all zip-code objects.
+
+    Returns
+    -------
+    list of dict
+        A list containing all zip-code objects.
+
+    >>> zipcodes.filter_by(zipcodes.list_all(), active=True, state='CT')
+    ... # all zip-codes for the state of Connecticut.
+
+    """
+    return zips
 
 
 def _contains_nondigits(s):
